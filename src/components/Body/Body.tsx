edd6308b56144abb6871a3816607da59b0533bba
l1rn <ivtaks06@gmail.com>
2026-05-14 02:25:06 +0500

    fix icons color in night mode & subtitle presentation in for mobiles

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -2,17 +2,58 @@ import './Body.scss'
 import { FaGithub, FaEnvelope, FaLinkedinIn } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 import Button from '../Button';
+import { useTheme } from '../../ThemeProvider';
+import { useEffect, useRef, useState } from 'react';
+
+function SubTitleItem( { 
+    text_main, text_addition, className, specificSign, isSmall 
+}: { 
+    text_main: string, 
+    text_addition: string,
+    className: string,
+    specificSign: string,
+    isSmall: boolean
+} ) {
+    return <div className={className}>
+        { isSmall ? 
+            text_main + specificSign + text_addition : 
+            <div>
+                <div>{text_main}</div>
+                <div>{text_addition}</div>
+            </div>
+        }
+    </div>
+}
+
 function Body() {
+    const { dark } = useTheme();
+    const [isSmall, setIsSmall] = useState(false);
+    useEffect(() => {
+        const media = window.matchMedia("(max-width: 768px)");
+
+        const update = () => setIsSmall(media.matches);
+
+        update();
+
+        media.addEventListener("change", update);
+
+        return () => media.removeEventListener("change", update);
+    }, [])
+
     return (
         <>
             <div className="body-wrapper">
                 <div className="body-container">
-                    <div className="body-title">
+                    <div className='body-title'>
                         l1rn
                     </div>
-                    <div className="body-subtitle">
-                        Full-stack developer • Java & React / Vue
-                    </div>
+                    <SubTitleItem 
+                        text_main='Full-stack developer '
+                        text_addition=' Java & React / Vue'
+                        specificSign='•'
+                        className="body-subtitle"
+                        isSmall={ !isSmall }
+                    />
 
                     <div className="line-separation"></div>
 
@@ -25,22 +66,22 @@ function Body() {
                         <ul>
                             <li>
                                 <Button onClick={() => window.open('https://github.com/l1rn', "_blank", "noopener,noreferrer")}>
-                                    <FaGithub size={32} />
+                                    <FaGithub size={32} color={dark ? "#f8fafc" : "#0f172a"} />
                                 </Button>
                             </li>
                             <li>
                                 <Button onClick={() => window.open('https://www.linkedin.com/in/l1rn/', "_blank", "noopener,noreferrer")}>
-                                    <FaLinkedinIn size={32} />
+                                    <FaLinkedinIn size={32} color={dark ? "#f8fafc" : "#0f172a"} />
                                 </Button>
                             </li>
                             <li>
                                 <Button onClick={() => window.open('https://leetcode.com/u/l1rn/', "_blank", "noopener,noreferrer")}>
-                                    <SiLeetcode size={32} />
+                                    <SiLeetcode size={32}  color={dark ? "#f8fafc" : "#0f172a"} />
                                 </Button>
                             </li>
                             <li>
                                 <Button>
-                                    <FaEnvelope size={32} />
+                                    <FaEnvelope size={32} color={dark ? "#f8fafc" : "#0f172a"} />
                                 </Button>
                             </li>
                         </ul>
